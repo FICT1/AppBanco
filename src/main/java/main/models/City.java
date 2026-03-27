1e6a7bcca93764a1e46c23d5466ff3848d4da22e
FICT1 <isaquitopro10@gmail.com>
2026-03-27 15:59:46 -0600

    Clases creadas

--- src/main/java/main/models/City.java
+++ src/main/java/main/models/City.java
@@ -1,4 +1,42 @@
 package main.models;
 
 public class City {
+    private String name;
+    private String descripcion;
+    private String code;
+
+    public City() {
+    }
+
+    public City(String name, String descripcion, String code) {
+        this.name = name;
+        this.descripcion = descripcion;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
 }
