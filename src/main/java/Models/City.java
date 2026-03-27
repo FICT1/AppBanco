3550520e629e9524b94cc3bdccdd1a1fdf2b5c9a
vybdani <darochap@uamv.edu.ni>
2026-03-27 15:59:48 -0600

    First Commit

---
+++ src/main/java/Models/City.java
@@ -0,0 +1,41 @@
+package Models;
+
+public class City {
+    private String name;
+    private String description;
+    private String code;
+
+    public City() {
+    }
+
+    public City(String name, String description, String code) {
+        this.name = name;
+        this.description = description;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
--- src/main/java/Models/main.java
+++ src/main/java/Models/main.java
@@ -1,4 +1,4 @@
-package main;
+package Models;
 
 public class main{
     public static void main(String[] args) {
